12444e88add7f23dd0bd1af51e1acefc7a6a71a3
nataliaventura93 <nataliaventura93@gmail.com>
2023-06-09 11:01:43 +0200

    Add text to DOM

--- main.js
+++ main.js
@@ -16,27 +16,6 @@ const labelMessageError = document.querySelector('.js-label-error');
 const input_search_desc = document.querySelector('.js_in_search_desc');
 const input_search_race = document.querySelector('.js_in_search_race');
 
-//Objetos con cada gatito
-/*
-const kittenData_1 = {
-  image: 'https://dev.adalab.es/gato-siames.webp',
-  name: 'Anastacio',
-  desc: 'Porte elegante, su patrón de color tan característico y sus ojos de un azul intenso, pero su historia se remonta a Asía al menos hace 500 años, donde tuvo su origen muy posiblemente.',
-  race: 'Siamés',
-};
-const kittenData_2 = {
-  image: 'https://dev.adalab.es/sphynx-gato.webp',
-  name: 'Fiona',
-  desc: 'Produce fascinación y curiosidad. Exótico, raro, bello, extraño… hasta con pinta de alienígena han llegado a definir a esta raza gatuna que se caracteriza por la «ausencia» de pelo.',
-  race: 'Sphynx',
-};
-const kittenData_3 = {
-  image: 'https://dev.adalab.es/maine-coon-cat.webp',
-  name: 'Cielo',
-  desc: ' Tienen la cabeza cuadrada y los ojos simétricos, por lo que su bella mirada se ha convertido en una de sus señas de identidad. Sus ojos son grandes y las orejas resultan largas y en punta.',
-  race: 'Maine Coon',
-};
-*/
 
 let kittenDataList = [];
 
@@ -61,7 +40,29 @@ if (kittenListStored) {
 }
 
 //Funciones
+
 function renderKitten(kittenData) {
+  const liElement = document.createElement('li');
+  //liElement.classList.add('list');
+  liElement.classList.add('card');
+  const liContent = document.createTextNode(`<article>
+  <img
+    class="card_img"
+    src=${kittenData.image}
+    alt="gatito"
+  />
+  <h3 class="card_title">${kittenData.name}</h3>
+  <h3 class="card_race">${kittenData.race}</h3>
+  <p class="card_description">
+  ${kittenData.desc}
+  </p>
+</article>`);
+liElement.appendChild(liContent);
+const list = document.querySelector('.list');
+
+list.appendChild(liElement);
+}
+/*function renderKitten(kittenData) {
   const kitten = `<li class="card">
     <article>
       <img
@@ -77,8 +78,7 @@ function renderKitten(kittenData) {
     </article>
     </li>`;
   return kitten;
-}
-
+}*/
 function renderKittenList(kittenDataList) {
   listElement.innerHTML = '';
   for (const kittenItem of kittenDataList) {
